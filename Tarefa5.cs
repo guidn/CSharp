a31baf975aa8650fd81d711b84844a3146447d64
guidn <78863010+guidn@users.noreply.github.com>
2023-10-25 22:31:48 -0300

    Add files via upload

---
+++ Tarefa5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Tarefa5
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Por favor, utilize virgula");
+			Console.WriteLine("Digite seu peso aqui");
+			Double peso = Double.Parse(Console.ReadLine());
+
+			Console.WriteLine("Digite sua altura aqui");
+			float altura = float.Parse(Console.ReadLine());
+
+			Double resultado = peso / (altura * altura);
+
+			Console.WriteLine("Seu IMC é de: " + resultado);
+
+			if (resultado <= 25)
+			{
+				Console.WriteLine("Seu peso está normal");
+			}
+			else
+			{
+				Console.WriteLine("Voce está acima do peso");
+			}
+
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+    }
+}
