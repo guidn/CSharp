78f84bd6acd1f3690d1d7521fcc636bf9903eb5c
guidn <78863010+guidn@users.noreply.github.com>
2023-10-25 22:34:23 -0300

    Add files via upload

---
+++ Tarefa4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Tarefa4
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Digite um numero");
+			int num = int.Parse(Console.ReadLine());
+
+			if (num % 3 == 0)
+			{
+				Console.WriteLine("multiplo por 3");
+			}
+			else
+			{
+				Console.WriteLine("NÃO é multiplo por 3");
+			}
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+    }
+}
